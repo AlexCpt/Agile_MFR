3e486d50875e1934416e39a538166a9719070169
Alex <alexandre.carpentier@insa-lyon.fr>
2017-11-20 08:23:50 +0100

    j'ai tout réparé

--- src/main/java/MapUI.java
+++ src/main/java/MapUI.java
@@ -2,11 +2,8 @@ import java.awt.*;
 import javax.swing.JPanel;
 
 public class MapUI extends JPanel {
-
-    Plan plan = new Plan();
-
+    
     public void paintComponent(Graphics g){
 
-        plan.afficher(g);
     }
 }
