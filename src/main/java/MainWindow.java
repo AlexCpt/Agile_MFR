6f74672a6868a901f8c19ffde5fbef992d93a465
Alex <alexandre.carpentier@insa-lyon.fr>
2017-11-22 10:29:35 +0100

    IHM

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,17 +1,27 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class MainWindow extends Application
 {
+
+    final int bandeauWidth = 200;
+    final int bandeauHeigth = 800;
     final int mapWidth = 800;
     final int mapHeight = 800;
+    final int sceneWidth = mapWidth+bandeauWidth ;
+    final int sceneHeight = mapHeight+bandeauHeigth;
+
     Plan plan;
     Tournee tournee;
 
@@ -19,7 +29,7 @@ public class MainWindow extends Application
     public MainWindow(){
         ParserXML parser = new ParserXML();
 
-        plan = parser.parsePlan("fichiersXML/planLyonGrand.xml");
+        plan = parser.parsePlan("fichiersXML/planLyonPetit.xml");
 
     }
 
@@ -31,14 +41,30 @@ public class MainWindow extends Application
 
     @Override
     public void start(Stage primaryStage){
-        primaryStage.setTitle("LE NOM DE NOTRE APPLI QUOI");
+        primaryStage.setTitle("HexaMaps 1.0");
+        primaryStage.setMinHeight(sceneHeight);
+        primaryStage.setMinWidth(sceneWidth);
 
+
+        //MapPane
         Pane mapPane = new Pane();
         mapPane.setStyle("-fx-background-color: #cccbc1;");
         mapPane.setPrefSize(mapWidth,mapHeight);
+        mapPane.setMinSize(mapWidth,mapHeight);
+        mapPane.setMaxHeight(mapHeight);
+        mapPane.setMaxWidth(mapWidth);
 
+        mapPane.setLayoutX(sceneWidth - mapWidth);
+        mapPane.setLayoutY(0);
+
+
+        //Label Title
+        Label lblTitle = new Label("Lyon - Livraison");
+        lblTitle.setLayoutY(100);
+
+        //Button
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Charger Livraison");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -46,12 +72,30 @@ public class MainWindow extends Application
             }
         });
 
+        //LeftHighVbox
+        VBox highVbox = new VBox();
+        highVbox.getChildren().add(lblTitle);
+        highVbox.setPrefSize(bandeauWidth, bandeauHeigth);
+        highVbox.setAlignment(Pos.CENTER);
+
+        //LeftBotVbox
+        VBox botVbox = new VBox();
+        botVbox.getChildren().add(btn);
+        botVbox.setPrefSize(bandeauWidth, bandeauHeigth);
+        botVbox.setAlignment(Pos.CENTER);
+
+        //Left Pane
+        BorderPane leftPane = new BorderPane();
+        leftPane.setTop(highVbox);
+        leftPane.setBottom(botVbox);
+
         plan.print(mapPane);
         //tournee.print(mapPane);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(mapPane);
-        root.getChildren().add(btn);
+        BorderPane root = new BorderPane();
+        root.setRight(mapPane);
+        root.setLeft(leftPane);
+
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
