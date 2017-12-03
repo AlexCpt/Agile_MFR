27ce164777ff1ed60b19511b0ec6218966fd763c
handroid <p.anh96@gmail.com>
2017-12-03 17:22:40 +0100

    update hover entrepot

--- src/main/java/fr/insalyon/agile/Tournee.java
+++ src/main/java/fr/insalyon/agile/Tournee.java
@@ -51,8 +51,6 @@ public class Tournee {
         return margesLivraison;
     }
 
-    public DemandeDeLivraison getDemandeDeLivraison(){return mDemandeDeLivraison;}
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
